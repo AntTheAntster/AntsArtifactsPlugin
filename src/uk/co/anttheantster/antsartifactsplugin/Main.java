d2905cd870e4db77feb81ccf2cc54fbe973efcd3
AntTheAntster <anttheantster@gmail.com>
2021-01-21 03:08:40 +0000

    Started with Artifact effects

--- src/uk/co/anttheantster/antsartifactsplugin/Main.java
+++ src/uk/co/anttheantster/antsartifactsplugin/Main.java
@@ -45,6 +45,9 @@ public class Main extends JavaPlugin {
         pm.registerEvents(new AddArtifactEffects(), this);
 
         registerEvents();
+
+
+
     }
 
     @Override
--- src/uk/co/anttheantster/antsartifactsplugin/listeners/AddArtifactEffects.java
+++ src/uk/co/anttheantster/antsartifactsplugin/listeners/AddArtifactEffects.java
@@ -1,20 +1,18 @@
 package uk.co.anttheantster.antsartifactsplugin.listeners;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
 import uk.co.anttheantster.antsartifactsplugin.artifacts.StrengthArtifact;
 
 public class AddArtifactEffects implements Listener {
 
-    public void strengthListener(PlayerInteractEvent e){
+    public void artifactListener(PlayerInteractEvent e){
         Player player = e.getPlayer();
 
         if (player.getInventory().containsAtLeast(StrengthArtifact.artifact, 1)){
-            player.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 1800, 1));
+            Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "effect " + player.getName() + " minecraft:strength 18000 2");
         }
     }
 
